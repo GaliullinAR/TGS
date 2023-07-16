28b52fe768c731ae13fab6485d604f733dc3540b
GaliullinAR <galiullin.ar651@gmail.com>
2023-07-16 06:47:59 +0300

    добавил функцию смены табов внизу страницы

--- src/js/app.js
+++ src/js/app.js
@@ -2,6 +2,7 @@ import $ from 'jquery';
 import "../../node_modules/slick-slider/slick/slick.css";
 import "../../node_modules/slick-slider/slick/slick.min.js";
 
+
 $('.slider-image').slick({
   arrows: false,
   dots: true,
@@ -9,6 +10,32 @@ $('.slider-image').slick({
   autoplaySpeed: 3000
 });
 
-const slickDots = document.querySelector('.slick-dots');
-const slickDotsItems = document.querySelectorAll('li');
+
+const navigation = document.querySelectorAll('.navigation__list-link');
+
+function getIndex() {
+  let idx = [...navigation].findIndex((el) => {
+    return el.parentElement.classList.contains("--active");
+  });
+
+  return idx;
+}
+
+navigation.forEach((item) => {
+
+  item.addEventListener('click', (e) => {
+    e.preventDefault();
+    const target = e.currentTarget.parentElement;
+
+    let index = getIndex();
+
+    navigation[index].parentElement.classList.remove('--active');
+    target.classList.add('--active');
+
+    const title = document.querySelector('.presentation__title');
+    title.textContent = target.textContent;
+
+  })
+})
+
 
